f679e80963636950d855263d2ca2b9c475fce316
Nathija Nimantha <nathija.max99@gmail.com>
2025-08-04 18:44:53 +0530

    init backend

---
+++ backend/src/main/java/com/airesume/resumescreeningtool/ResumeScreeningToolApplication.java
@@ -0,0 +1,13 @@
+package com.airesume.resumescreeningtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeScreeningToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeScreeningToolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/airesume/resumescreeningtool/ResumeScreeningToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airesume.resumescreeningtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeScreeningToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
