2da7c4d6a713c053dcd6287d9b9e44c4c1aa0923
nathija-nimantha <nathija.max99@gmail.com>
2025-08-10 19:52:53 +0530

    feat: add logging to application startup in ResumeScreeningToolApplication

--- backend/src/main/java/com/airesume/resumescreeningtool/ResumeScreeningToolApplication.java
+++ backend/src/main/java/com/airesume/resumescreeningtool/ResumeScreeningToolApplication.java
@@ -1,13 +1,15 @@
 package com.airesume.resumescreeningtool;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ResumeScreeningToolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResumeScreeningToolApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeScreeningToolApplication.class, args);
+        log.info("Resume Screening Tool Application has started successfully.");
+    }
 }
